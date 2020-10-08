e75317909c0f66a0fb80d1dd51cfb28329775b81
dusarah <sarahdufite@gmail.com>
2020-10-08 18:13:54 +0200

    i have added script for design iccon script for development icon and script product icon

--- js/script.js
+++ js/script.js
@@ -4,8 +4,24 @@ $(document).ready(function(){
     $(this).hide();
     $('p.paragraph').show();
   });
-  $('p.paragraph').click(function()
-{
+  $('p.paragraph').click(function(){
   $(this).hide();
   $('p.icon').show();
+});
+//development icon interface
+$('#second p.dev').click(function(){
+  $(this).hide();
+  $('p.para').show();
+});
+$('p.para').click(function(){
+$(this).hide();
+$('p.dev').show();
+//product icon dev_icon
+$('#second p.product').click(function(){
+  $(this).hide();
+  $('p.parag').show();
+});
+$('p.parag').click(function(){
+$(this).hide();
+$('p.product').show();
 });
